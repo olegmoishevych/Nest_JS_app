34c644997ae7dbd5a263f66225319e276ddbc387
Samurai-way <olegmoishevych@gmail.com>
2023-03-03 11:14:35 +0200

    fix: bugs

--- src/modules/testing/controller/testing.controller.ts
+++ src/modules/testing/controller/testing.controller.ts
@@ -1,11 +1,12 @@
 import { TestingService } from '../service/testing.service';
-import { Controller, Delete } from '@nestjs/common';
+import { Controller, Delete, HttpCode } from '@nestjs/common';
 
 @Controller('api')
 export class TestingController {
   constructor(private testingService: TestingService) {}
 
   @Delete('testing/all-data')
+  @HttpCode(204)
   async deleteAllData() {
     return this.testingService.deleteAllData();
   }
--- src/modules/testing/repository/testing.repository.ts
+++ src/modules/testing/repository/testing.repository.ts
@@ -3,16 +3,27 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Users, UsersDocument } from '../../users/schemas/users.schema';
 import { Model } from 'mongoose';
 import { Blogs, BlogsDocument } from '../../blogs/schemas/blogs.schema';
+import {
+  Comments,
+  CommentsDocument,
+} from '../../comments/schema/comments.schema';
 
 @Injectable()
 export class TestingRepository {
   constructor(
     @InjectModel(Blogs.name) private readonly blogsModel: Model<BlogsDocument>,
     @InjectModel(Users.name) private readonly usersModel: Model<UsersDocument>,
+    @InjectModel(Comments.name)
+    private readonly commentsModel: Model<CommentsDocument>,
   ) {}
   async deleteAllData(): Promise<boolean> {
     const clearBlogs = await this.blogsModel.deleteMany({});
     const clearUsers = await this.usersModel.deleteMany({});
-    return clearBlogs.deletedCount === 1 && clearUsers.deletedCount === 1;
+    const clearComments = await this.usersModel.deleteMany({});
+    return (
+      clearBlogs.deletedCount === 1 &&
+      clearUsers.deletedCount === 1 &&
+      clearComments.deletedCount === 1
+    );
   }
 }
