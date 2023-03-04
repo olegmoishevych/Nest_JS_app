ebfb1a871190c7c0ac0afb94f85f55f38aecebbe
Samurai-way <olegmoishevych@gmail.com>
2023-03-04 21:50:14 +0200

    feat: search users login and email

--- src/modules/auth/repository/auth.repository.ts
+++ src/modules/auth/repository/auth.repository.ts
@@ -8,4 +8,10 @@ export class AuthRepository {
   constructor(
     @InjectModel(Users.name) private readonly usersModel: Model<UsersDocument>,
   ) {}
+  async findUserByLogin(login: string) {
+    return this.usersModel.findOne({ login });
+  }
+  async findUserByEmail(email: string) {
+    return this.usersModel.findOne({ email });
+  }
 }
--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AuthRepository } from '../repository/auth.repository';
 import { AuthDto } from '../dto/auth.dto';
 
 @Injectable()
 export class AuthService {
   constructor(public authRepository: AuthRepository) {}
-  async userRegistration(registrationDto: AuthDto) {}
+
+  async userRegistration(registrationDto: AuthDto) {
+    const findUserByLogin = await this.authRepository.findUserByLogin(
+      registrationDto.login,
+    );
+    if (!findUserByLogin)
+      throw new NotFoundException([
+        {
+          message: 'Login not found',
+          field: 'login',
+        },
+      ]);
+    const findUserByEmail = await this.authRepository.findUserByEmail(
+      registrationDto.email,
+    );
+    if (!findUserByEmail)
+      throw new NotFoundException([
+        {
+          message: 'Email not found',
+          field: 'email',
+        },
+      ]);
+  }
 }
