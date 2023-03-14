74257a1ee85dbfc5097e0e41e69d3b05c1e3d7ed
Samurai-way <olegmoishevych@gmail.com>
2023-03-14 23:38:13 +0200

    fix: bugs

--- src/modules/blogs/service/blogs.service.ts
+++ src/modules/blogs/service/blogs.service.ts
@@ -194,14 +194,14 @@ export class BlogsService {
     pagination: BannedUserDto,
     userId: string,
   ): Promise<PaginationViewModel<BlogsUserViewModel[]>> {
-    const blogById = await this.blogsRepository.findBlogById(blogId);
+    const blogById = await this.blogsRepository.findBlogWithOwnerId(blogId);
     if (!blogById) throw new NotFoundException(['Blog not found']);
+    if (blogById.blogOwnerInfo.userId !== userId)
+      throw new ForbiddenException([]);
     const bannedUser = await this.userBannedRepository.findBannedUserByBlogId(
       blogId,
     );
     if (!bannedUser) throw new NotFoundException(['User by id not found']);
-    // if (blogById.blogOwnerInfo.userId !== userId)
-    //   throw new ForbiddenException([]);
     return this.userBannedRepository.getBannedUsersForBlog(blogId, pagination);
   }
   async banBlogById(blogId: string, isBanned: boolean): Promise<boolean> {
