890cac38c98f1098d4af5633405accdc90e4059c
Samurai-way <olegmoishevych@gmail.com>
2023-03-16 21:52:05 +0200

    feat: fix sentEmail service

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -4,13 +4,13 @@ import { MailerService } from '@nest-modules/mailer';
 @Injectable()
 export class EmailService {
   constructor(private readonly mailerService: MailerService) {}
+
   async sentEmail(email: string, subject: string, text: string) {
-    const sendEmail = await this.mailerService.sendMail({
+    return this.mailerService.sendMail({
       from: 'Oleg <olegmoishevych@gmail.com>',
       to: email,
       subject,
       text,
     });
-    return;
   }
 }
