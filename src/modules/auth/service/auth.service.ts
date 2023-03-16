799551bddc874884f9f420812230fd5f2683b8e4
Samurai-way <olegmoishevych@gmail.com>
2023-03-16 15:05:57 +0200

    fix: bugs

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -152,11 +152,11 @@ export class AuthService {
     const payload = { userId: userId, deviceId: deviceId };
     const jwtPair: JwtPairType = {
       accessToken: this.jwtService.sign(payload, {
-        expiresIn: '10000s',
+        expiresIn: '10s',
         secret: JWT.jwt_secret,
       }),
       refreshToken: this.jwtService.sign(payload, {
-        expiresIn: '20000s',
+        expiresIn: '20s',
         secret: JWT.jwt_secret,
       }),
     };
