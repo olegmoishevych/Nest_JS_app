7e6d23938d65ac4fe7e1381f80c6759f60f44bb5
Samurai-way <olegmoishevych@gmail.com>
2023-03-20 17:42:32 +0200

    feat: LoginCommand logic

--- src/modules/auth/use-cases/login.use-case.ts
+++ src/modules/auth/use-cases/login.use-case.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CommandHandler } from '@nestjs/cqrs';
+import { UserEntity } from '../domain/entities/user.entity';
 
 @Injectable()
 export class LoginCommand {
-  constructor() {}
+  constructor(
+    readonly ip: string,
+    readonly title: string,
+    readonly user: UserEntity,
+  ) {}
 }
 
 @CommandHandler(LoginCommand)
 export class LoginUseCase {
   constructor() {}
-  async execute(command: any) {}
+
+  async execute(command: LoginCommand) {
+    const { user, title, ip } = command;
+    if (user.banInfo.isBanned)
+      throw new UnauthorizedException(['User is banned']);
+  }
 }
