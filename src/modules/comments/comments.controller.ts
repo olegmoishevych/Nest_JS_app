9d4da80f5bdf6bfff36d6a2d07d4440f2ec4dd0c
Samurai-way <olegmoishevych@gmail.com>
2023-03-24 18:15:23 +0200

    fix: bugs

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -8,10 +8,8 @@ import {
   Put,
   UseGuards,
 } from '@nestjs/common';
-import { CommentsService } from './service/comments.service';
 import { CommentsViewModal } from './schema/comments.schema';
 import { User } from '../auth/decorator/request.decorator';
-import { UserModel } from '../users/schemas/users.schema';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CommentsDto } from './dto/comments.dto';
 import { LikeStatusDto } from './dto/likeStatus.dto';
@@ -28,10 +26,7 @@ import { CommentsEntity } from './domain/comments.entity';
 
 @Controller('comments')
 export class CommentsController {
-  constructor(
-    public commentsService: CommentsService,
-    public command: CommandBus,
-  ) {}
+  constructor(public command: CommandBus) {}
 
   @Get('/:id')
   async findCommentById(
