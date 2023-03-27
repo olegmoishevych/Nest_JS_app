75d015bd1d05ff354852736395f382e374098788
Samurai-way <olegmoishevych@gmail.com>
2023-03-27 19:20:19 +0300

    feat: fix bugs

--- src/modules/testing/repository/testing.sql.repository.ts
+++ src/modules/testing/repository/testing.sql.repository.ts
@@ -1,31 +1,59 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentsEntity } from '../../comments/domain/comments.entity';
+import { UserEntity } from '../../auth/domain/entities/user.entity';
 
 @Injectable()
 export class TestingSqlRepository {
-  constructor(private dataSource: DataSource) {}
+  constructor(
+    @InjectRepository(CommentsEntity)
+    private usersTable: Repository<UserEntity>,
+    private dataSource: DataSource,
+  ) {}
+
+  // async deleteAll(): Promise<boolean> {
+  //   try {
+  //     const entities = this.dataSource.entityMetadatas;
+  //     const tableNames = entities
+  //       .map((entity) => `"${entity.tableName}"`)
+  //       .join(', ');
+  //
+  //     await this.dataSource.query(`TRUNCATE ${tableNames} CASCADE;`);
+  //     return true;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
 
   async deleteAllData() {
-    // await this.usersTable.delete({});
     try {
-      await this.dataSource.query(`
-        CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
-DECLARE
-    statements CURSOR FOR
-        SELECT tablename FROM pg_tables
-        WHERE tableowner = username AND schemaname = 'public';
-BEGIN
-    FOR stmt IN statements LOOP
-        EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
-    END LOOP;
-END;
-$$ LANGUAGE plpgsql;
-SELECT truncate_tables('postgres');
-SELECT truncate_tables('neondb');`);
+      await this.usersTable.delete({});
       return true;
     } catch (e) {
       console.log(e);
       return null;
     }
+    //     try {
+    //       await this.dataSource.query(`
+    //         CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
+    // DECLARE
+    //     statements CURSOR FOR
+    //         SELECT tablename FROM pg_tables
+    //         WHERE tableowner = username AND schemaname = 'public';
+    // BEGIN
+    //     FOR stmt IN statements LOOP
+    //         EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+    //     END LOOP;
+    // END;
+    // $$ LANGUAGE plpgsql;
+    // SELECT truncate_tables('postgres');
+    // SELECT truncate_tables('neondb');`);
+    //       return true;
+    //     } catch (e) {
+    //       console.log(e);
+    //       return null;
+    //     }
+    //   }
   }
 }
