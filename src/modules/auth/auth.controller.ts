0d883502ce969436b8032988a9f6684110a12447
Samurai-way <olegmoishevych@gmail.com>
2023-03-29 09:30:25 +0300

    fix: bugs

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -56,7 +56,6 @@ export class AuthController {
   ): Promise<boolean> {
     return this.commandBus.execute(new EmailResendingCommand(email));
   }
-  // console.log()
   @UseGuards(LocalAuthGuard)
   @Throttle(5, 10)
   @HttpCode(200)
